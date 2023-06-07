59bdd2daa9798ac073a0576044eae01ab9f6c032
George Dang <53052793+gtdang@users.noreply.github.com>
2023-06-07 11:17:18 -0400

    Update combine_csv_files script to match what is in the notebook.

--- src/data/utils.py
+++ src/data/utils.py
@@ -4,15 +4,18 @@ import pandas as pd
 
 def combine_csv_files(path, prefix, suffix='.csv', **kwargs):
     """
-    This function searches a directory for text files, imports them as pandas.DataFrames and
+    Searches a directory for text files, imports as pandas.DataFrames and
     concatenates to a single DataFrame.
-    :param path: Path object to a directory
-    :param prefix: Prefix to search for
-    :param suffix: Suffix to search for, Default '.csv'
-    :param kwargs: Additional arguments to give to the pandas.read_csv function
-    :return: DataFrame
-    """
 
+    Args:
+        path: pathlib.Path object of a directory
+        prefix: String prefix to search for
+        suffix: String suffix to search for, Optional, Default '.csv'
+        **kwargs: Variable additional arguments pass to the pandas.read_csv function
+
+    Returns: pandas.DataFrame
+
+    """
     # List files in the directory
     list_files_in_path = sorted(os.listdir(path))
 
@@ -21,7 +24,7 @@ def combine_csv_files(path, prefix, suffix='.csv', **kwargs):
 
     # Loop through files
     for file in list_files_in_path:
-        # Check if the file starts with the prefix and ends with csv
+        # Check if the file starts with the prefix and ends with the suffix
         if file.startswith(prefix) and file.endswith(suffix):
             # Read in csv as DataFrame and append it to the list
             list_df.append(pd.read_csv(path / file, **kwargs))
@@ -29,7 +32,6 @@ def combine_csv_files(path, prefix, suffix='.csv', **kwargs):
             continue
 
     # Concatenate the DataFrames
-    df_return = pd.concat(list_df)
-    df_return.reset_index(drop=True, inplace=True)
+    df_return = pd.concat(list_df).reset_index(drop=True)
 
     return df_return
